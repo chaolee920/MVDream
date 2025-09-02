2357f2dd13c351839516ed0825a22a62d5cb56d6
mac <mac@MacMini-KJS-1.local>
2025-09-02 16:54:53 +0700

    feat: image saving

--- scripts/t2i.py
+++ scripts/t2i.py
@@ -98,7 +98,7 @@ if __name__ == "__main__":
         img = t2i(model, args.size, t, uc, sampler, step=50, scale=10, batch_size=batch_size, ddim_eta=0.0, 
                 dtype=dtype, device=device, camera=camera, num_frames=args.num_frames)
         for k in range(len(img)):
-            print(img[k])
+            Image.fromarray(img[k]).save(f"sample{k}.png")
         img = np.concatenate(img, 1)
         images.append(img)
     images = np.concatenate(images, 0)
